c587a8f7d5c9792df8f53dbbfbf536f000222784
Prashant <prashantsaroha13@gmail.com>
2024-06-30 15:31:54 +0530

    just check commit

---
+++ manga.py
@@ -0,0 +1,26 @@
+import requests
+
+def download_manga(name,url):
+     pass
+
+def chapter_link(url):
+    pass
+    
+
+
+
+def main():
+    url=input("please enter the url of manga :")
+    print("url" +url)
+
+    chapters=chapter_link(url)
+    for chapter in chapters:
+        print(chapter +":" +chapters[chapter])
+        ask=input("do you want to download it (y/n)")
+        if ask=="y":
+            download_manga(chapter,chapters[chapter])
+
+
+        else:
+            continue  
+
